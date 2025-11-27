a50607a1feaa401717de6974ec6339657790334a
Radcabna <radcabna@gmail.com>
2025-11-27 21:33:15 +0300

    Initial Commit

---
+++ Plunka Memory Weathe/Plunka_Memory_WeatheApp.swift
@@ -0,0 +1,17 @@
+//
+//  Plunka_Memory_WeatheApp.swift
+//  Plunka Memory Weathe
+//
+//  Created by Алкександр Степанов on 27.11.2025.
+//
+
+import SwiftUI
+
+@main
+struct Plunka_Memory_WeatheApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
